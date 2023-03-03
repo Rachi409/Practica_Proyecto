9587b2b4d3b6a7a0f37a6cbf144e2f1ae209a002
Rachi409 <126910096+Rachi409@users.noreply.github.com>
2023-03-03 18:16:53 -0400

    Add files via upload

---
+++ lib/Pages/menu_page.dart
@@ -0,0 +1,430 @@
+import 'package:flutter/material.dart';
+
+class menu_page extends StatelessWidget {
+  const menu_page({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.red,
+          centerTitle: true,
+          title: Text('BIENVENIDO'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                //cuadro1(),
+                Container(
+                  padding: EdgeInsets.all(10),
+                  height: 300,
+                  width: 300,
+                  color: Colors.transparent,
+                  child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          elevation: 100,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(50)),
+                          primary: Colors.pink),
+                      onPressed: () {
+                        Navigator.of(context).pushNamed('/two');
+                      },
+                      child: Text('PAGE 4')),
+                ),
+                //cuadro2()
+                Container(
+                  padding: EdgeInsets.all(10),
+                  height: 300,
+                  width: 300,
+                  color: Colors.transparent,
+                  child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          elevation: 100,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(50)),
+                          primary: Colors.black),
+                      onPressed: () {
+                        Navigator.of(context).pushNamed('/three');
+                      },
+                      child: Text('PAGE 3')),
+                )
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                //cuadro3(),
+                Container(
+                  padding: EdgeInsets.all(10),
+                  height: 300,
+                  width: 300,
+                  color: Colors.transparent,
+                  child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          elevation: 100,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(50)),
+                          primary: Colors.indigo),
+                      onPressed: () {
+                        Navigator.of(context).pop('/two');
+                      },
+                      child: Text('PAGE 2')),
+                ),
+                //cuadro4()
+                Container(
+                  padding: EdgeInsets.all(10),
+                  height: 300,
+                  width: 300,
+                  color: Colors.transparent,
+                  child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          elevation: 100,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(50)),
+                          primary: Colors.amber),
+                      onPressed: () {
+                        Navigator.of(context).pop('/four');
+                      },
+                      child: Text('PAGE 1')),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class cuadro1 extends StatelessWidget {
+  const cuadro1({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(10),
+      height: 300,
+      width: 300,
+      color: Colors.transparent,
+      child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+              elevation: 100,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(50)),
+              primary: Colors.pink),
+          onPressed: () {
+            Navigator.of(context).pushNamed('/two');
+          },
+          child: Text('PAGE 4')),
+    );
+  }
+}
+
+class cuadro2 extends StatelessWidget {
+  const cuadro2({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(10),
+      height: 300,
+      width: 300,
+      color: Colors.transparent,
+      child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+              elevation: 100,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(50)),
+              primary: Colors.black),
+          onPressed: () {
+            Navigator.of(context).pushNamed('/three');
+          },
+          child: Text('PAGE 3')),
+    );
+  }
+}
+
+class cuadro3 extends StatelessWidget {
+  const cuadro3({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(10),
+      height: 300,
+      width: 300,
+      color: Colors.transparent,
+      child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+              elevation: 100,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(50)),
+              primary: Colors.indigo),
+          onPressed: () {
+            Navigator.of(context).pop('/one');
+          },
+          child: Text('PAGE 2')),
+    );
+  }
+}
+
+class cuadro4 extends StatelessWidget {
+  const cuadro4({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(10),
+      height: 300,
+      width: 300,
+      color: Colors.transparent,
+      child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+              elevation: 100,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(50)),
+              primary: Colors.amber),
+          onPressed: () {
+            Navigator.of(context).pop('/four');
+          },
+          child: Text('PAGE 1')),
+    );
+  }
+}
+
+/*
+import 'package:flutter/material.dart';
+
+class menu_page extends StatelessWidget {
+  const menu_page({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.red,
+          centerTitle: true,
+          title: Text('BIENVENIDO'),
+        ),
+        body: Column(
+          children: <Widget>[
+            SizedBox(
+              height: 50,
+            ),
+            Expanded(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  //cuadro1(),
+                  Expanded(
+                    child: Container(
+                      height: 500,
+                      padding: EdgeInsets.all(5),
+                      color: Colors.black,
+                      child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(30)),
+                              primary: Colors.pink),
+                          onPressed: () {
+                            Navigator.of(context).pushNamed('/two');
+                          },
+                          child: Text('PAGE 4')),
+                    ),
+                  ),
+                  //cuadro2()
+                  Expanded(
+                    child: Container(
+                      height: 500,
+                      padding: EdgeInsets.all(5),
+                      color: Colors.transparent,
+                      child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              elevation: 100,
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(50)),
+                              primary: Colors.black),
+                          onPressed: () {
+                            Navigator.of(context).pushNamed('/three');
+                          },
+                          child: Text('PAGE 3')),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Expanded(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  //cuadro3(),
+                  Expanded(
+                    child: Container(
+                      height: 500,
+                      padding: EdgeInsets.all(5),
+                      color: Colors.transparent,
+                      child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              elevation: 100,
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(50)),
+                              primary: Colors.indigo),
+                          onPressed: () {
+                            Navigator.of(context).pushNamed('/two');
+                          },
+                          child: Text('PAGE 2')),
+                    ),
+                  ),
+                  //cuadro4()
+                  Expanded(
+                    child: Container(
+                      height: 500,
+                      padding: EdgeInsets.all(5),
+                      color: Colors.transparent,
+                      child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              elevation: 100,
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(30)),
+                              primary: Colors.amber),
+                          onPressed: () {
+                            Navigator.of(context).pushNamed('/four');
+                          },
+                          child: Text('PAGE 1')),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 40,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class cuadro1 extends StatelessWidget {
+  const cuadro1({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(10),
+      height: 300,
+      width: 300,
+      color: Colors.transparent,
+      child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+              elevation: 100,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(50)),
+              primary: Colors.pink),
+          onPressed: () {
+            Navigator.of(context).pushNamed('/two');
+          },
+          child: Text('PAGE 4')),
+    );
+  }
+}
+
+class cuadro2 extends StatelessWidget {
+  const cuadro2({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(10),
+      height: 300,
+      width: 300,
+      color: Colors.transparent,
+      child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+              elevation: 100,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(50)),
+              primary: Colors.black),
+          onPressed: () {
+            Navigator.of(context).pushNamed('/three');
+          },
+          child: Text('PAGE 3')),
+    );
+  }
+}
+
+class cuadro3 extends StatelessWidget {
+  const cuadro3({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(10),
+      height: 300,
+      width: 300,
+      color: Colors.transparent,
+      child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+              elevation: 100,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(50)),
+              primary: Colors.indigo),
+          onPressed: () {
+            Navigator.of(context).pop('/one');
+          },
+          child: Text('PAGE 2')),
+    );
+  }
+}
+
+class cuadro4 extends StatelessWidget {
+  const cuadro4({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(10),
+      height: 300,
+      width: 300,
+      color: Colors.transparent,
+      child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+              elevation: 100,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(50)),
+              primary: Colors.amber),
+          onPressed: () {
+            Navigator.of(context).pop('/four');
+          },
+          child: Text('PAGE 1')),
+    );
+  }
+}
+*/
---
+++ lib/Pages/second_page.dart
@@ -0,0 +1,166 @@
+import 'package:flutter/material.dart';
+
+class second_page extends StatelessWidget {
+  // int currentPage = 0;
+  // final PageController pageController = new PageController(initialPage: 1);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          leading: IconButton(onPressed: Navigator.of(context).pop, icon: Icon(Icons.key_rounded)),
+          backgroundColor: Colors.red,
+          title: Text('Hola Papu'),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(
+                height: 300,
+                padding: EdgeInsets.only(bottom: 15),
+                //width: 1500,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: NetworkImage(
+                        'https://upload.wikimedia.org/wikipedia/commons/d/d4/Edificios_en_Equipetrol%2C_Santa_Cruz_de_la_Sierra%2C_Bolivia.jpg'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: <Widget>[
+                      Container(
+                        height: 100,
+                        width: 100,
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                              image: NetworkImage(
+                                  'https://e.rpp-noticias.io/normal/2020/11/21/500050_1025639.jpg'),
+                              fit: BoxFit.cover),
+                          shape: BoxShape.circle,
+                          border: Border.all(color: Colors.white, width: 3),
+                        ),
+                      ),
+                      Text(
+                        '@ElBichoSiu',
+                        style: TextStyle(color: Colors.white, inherit: true),
+                      ),
+                    ]),
+              ),
+              Container(
+                height: 100,
+                color: Colors.red,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Column(
+                      children: <Widget>[
+                        Text(
+                          'Siguiendo',
+                          style: TextStyle(
+                            fontSize: 40,
+                            fontStyle: FontStyle.italic,
+                            color: Colors.white,
+                          ),
+                        ),
+                        Text(
+                          '77',
+                          style: TextStyle(
+                            fontSize: 40,
+                            fontStyle: FontStyle.italic,
+                            color: Colors.white,
+                          ),
+                        )
+                      ],
+                    ),
+                    Column(
+                      children: <Widget>[
+                        Text(
+                          'Seguidores',
+                          style: TextStyle(
+                            fontSize: 35,
+                            fontStyle: FontStyle.italic,
+                            color: Colors.white,
+                          ),
+                        ),
+                        Text(
+                          '500 M',
+                          style: TextStyle(
+                            fontSize: 35,
+                            fontStyle: FontStyle.italic,
+                            color: Colors.white,
+                          ),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(20),
+                child: Column(
+                  children: <Widget>[
+                    Text(
+                        ' animales, pero no con insectosEl astro portugués se ganó el curioso sobrenombre cuando jugaba para el Real Madrid. El delantero portugués Cristiano Ronaldo es conocido por sus imponentes saltos, sus excelentes instintos en el área y sus poderosos disparos, características que uno asociaría con la fuerza de muchos animales, pero no con insectosEl astro portugués se ganó el curioso sobrenombre cuando jugaba para el Real Madrid. El delantero portugués Cristiano Ronaldo es conocido por sus imponentes saltos, sus excelentes instintos en el área y sus poderosos disparos, características que uno asociaría con la fuerza de muchos animales, pero no con insectosEl astro portugués se ganó el curioso sobrenombre cuando jugaba para el Real Madrid. El delantero portugués Cristiano Ronaldo es conocido por sus imponentes saltos, sus excelentes instintos en el área y sus poderosos disparos, características que uno asociaría con la fuerza de muchos animales, pero no con insectosEl astro portugués se ganó el curioso sobrenombre cuando jugaba para el Real Madrid. El delantero portugués Cristiano Ronaldo es conocido por sus imponentes saltos, sus excelentes instintos en el área y sus poderosos disparos, características que uno asociaría con la fuerza de muchos animales, pero no con insectos.')
+                  ],
+                ),
+              ),
+              Container(
+                height: 230,
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      child: FloatingActionButton(
+                        child: Text('Go Page 1'),
+                        onPressed: () {
+                          Navigator.of(context).pushNamed('/one');
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          //      currentIndex: currentPage,
+          //      onTap: (index){
+          //        currentPage=index;
+          //        pageController.animateToPage(index, duration: Duration(seconds: 2), curve: Curves.bounceOut);
+          //      },
+
+          backgroundColor: Colors.red,
+          selectedItemColor: Colors.black,
+          unselectedItemColor: Colors.white,
+          items: [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.r_mobiledata_sharp), label: 'Richard'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.access_alarm), label: 'Alarm'),
+            BottomNavigationBarItem(icon: Icon(Icons.cable), label: 'Cronom'),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Boton2to1 extends StatelessWidget {
+  const Boton2to1({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+      child: Text('Go Page 1'),
+      onPressed: () {
+        Navigator.of(context).pushNamed('/three');
+      },
+    );
+  }
+}
